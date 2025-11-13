bd6db5b0a0e50825b5172cca4362e80b162c55cb
Fad Rahim <fadrahim1@gmail.com>
2025-11-13 10:59:34 +0800

    Initial Commit

---
+++ Recall/RecallApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecallApp.swift
+//  Recall
+//
+//  Created by Fad Rahim on 13/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RecallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RecallTests/RecallTests.swift
@@ -0,0 +1,17 @@
+//
+//  RecallTests.swift
+//  RecallTests
+//
+//  Created by Fad Rahim on 13/11/25.
+//
+
+import Testing
+@testable import Recall
+
+struct RecallTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
